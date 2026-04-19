25bbff127c33655f604ef3de7eda4454f0b2820f
tainanafeng <tainanafeng@gmail.com>
2026-04-20 00:27:45 +0800

    Update users_account.py

--- config/users_account.py
+++ config/users_account.py
@@ -9,19 +9,5 @@ if TEST_USERS_JSON:
 else:
     # fallback：本機 / demo 預設帳號
     TEST_USERS = {
-        "user1":  "Q8tL92FbW1",
-        "user2":  "mK47pTz81B",
-        "user3":  "Xv91LqT2sE",
-        "user4":  "R7bYw38KpQ",
-        "user5":  "gC52ZLm8tR",
-        "user6":  "Tn84vBs93W",
-        "user7":  "Hs67WqP49J",
-        "user8":  "J3cPzL81tV",
-        "user9":  "Lw95NqR27B",
-        "user10": "Zt28HsQ56M",
-        "manager": "test_123",
-        "mom": "test_123",
-        "dad": "test_123",
-        "professor": "test_123",
-        "viewer": "test_123",
+        "nycu":  "nycu",
     }
